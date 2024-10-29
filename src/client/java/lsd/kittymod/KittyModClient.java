a07200d1a55b879ff47b1d930f20ae06dbfa3318
Alanik <lilia.dostie@socprep.org>
2024-10-29 16:23:14 -0400

    Initial commit

---
+++ src/client/java/lsd/kittymod/KittyModClient.java
@@ -0,0 +1,10 @@
+package lsd.kittymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KittyModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/lsd/kittymod/KittyModDataGenerator.java
@@ -0,0 +1,11 @@
+package lsd.kittymod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KittyModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
